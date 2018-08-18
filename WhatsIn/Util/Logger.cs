1876c3daedcf498b4d5ac5355155622e33aaf6df
Rodel Simangan <rodelsimangan@gmail.com>
2018-08-17 20:44:22 -0400

    8-17-18

---
+++ WhatsIn/Util/Logger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WhatsIn.Util
+{    public static class Logger
+    {
+        public static void LogError(Type source, Exception ex)
+        {
+            log4net.ILog logger = log4net.LogManager.GetLogger(source);
+            logger.Error(ex.ToString());
+        }
+
+        public static void LogInfo(Type source, string message)
+        {
+            log4net.ILog logger = log4net.LogManager.GetLogger(source);
+            logger.Info(message);
+        }
+
+        public static void LogDebug(Type source, string message)
+        {
+            log4net.ILog logger = log4net.LogManager.GetLogger(source);
+            logger.Debug(message);
+        }
+    }
+}
